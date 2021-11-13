4d46933c15230d38ae0cfe3c3f5d6ea743be0d9b
santiago <santiago.gallego1@utp.edu.co>
2021-11-13 16:16:17 -0500

    clase objetos

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const natalia = {
+
+    name: "natalia",
+    age: 20,
+    cursosAprobados: ["curso de html y css",
+"curso practico de html y css", ],
+};
+
+
+
+// hacer que natalia apruebe el curso
+
+natalia.cursosAprobados.push("responsive design")
+
+console.log(natalia)

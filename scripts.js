14983cc60a52bf484e56a6abd3c255ab073d292c
santiago <santiago.gallego1@utp.edu.co>
2021-11-13 21:15:37 -0500

    Estamos en la practica de objetis con clases

--- scripts.js
+++ scripts.js
@@ -1,10 +1,39 @@
 const natalia = {
 
     name: "natalia",
-    age: 20,
+    agee: 20,
     cursosAprobados: ["curso de html y css",
 "curso practico de html y css", ],
+
+aprobarCurso: function ( nuevoCurso){
+
+    this.cursosAprobados.push(nuevoCurso);
+
+
+}
 };
+
+
+
+function Student(name, age, cursosAprobados){
+
+    this.name = name;
+    this.age = age;
+    this.cursosAprobados = cursosAprobados
+    this.aprobarCurso = function (cursoNuevo){
+
+        this.cursosAprobados.push(cursoNuevo);
+    };
+
+}
+
+Student.prototype.aprobarCurso = function(curso){
+
+    return this.cursosAprobados.push(curso);
+}
+
+
+const santiago = new Student("santiago", 23,["matematicas","sistemas"]);
 
 
 
@@ -13,3 +42,35 @@ const natalia = {
 natalia.cursosAprobados.push("responsive design")
 
 console.log(natalia)
+
+
+
+
+
+
+class estudiante{
+
+    
+constructor(nombre, edad, cursos){
+
+    this.nombre = nombre;
+    this.edad = edad;
+    this.cursos = cursos;
+    
+}
+
+
+
+ meterCursos(cursos){
+
+    this.cursos += " "+cursos;
+
+ }
+
+
+
+}
+
+
+
+const estudiante1 = new estudiante("kevin",24,"matematicas");
